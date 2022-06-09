500dcfa796ff81d41b3f2fb180eb3cb3746d7713
1_b1n <81698301+wb96choi@users.noreply.github.com>
2022-06-09 12:33:46 +0900

    0609에 한 image gallery 사이트 실습
    0609에 한 image gallery 사이트 실습

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.img-thumb li').click(function(){
+        // this: 부모 li 중 사용자가 클릭한 li
+        // 그 밑에 이미지 src값읽음
+        var thisSrc = $(this).children().attr('src');
+        // 메인이미지 변경
+        $('.main-image img').attr('src', thisSrc);
+    });
+
+}); 
